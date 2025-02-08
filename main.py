9efc81d52202b7925beb23c8f5c7e97cfaa5add1
unknown <atorralb@outlook.com>
2025-02-08 13:16:15 -0600

    new env

---
+++ main.py
@@ -0,0 +1,10 @@
+#Verifica comprobante de pago
+#Cuando vendes tu automovil, el estafador por lo regular quiere intimidarte con un comprobante electronico de pago falso
+#te envian un screenshot de que ya te depositaron y que te piden que verifiques en banxico
+#tu te metes a banxico, te da la informacion de un pago, pero no te da un Comprobante Electronico de Pago (CEP)
+#sEsto quiere decir que el defraudador hace un movimiento a otra cuenta del mismo banco que tu tienes
+#por medio de la clave de rastreo, tu puedes bajar la informacion de pago, pero no puedes bajar el comprobante
+#por que ese pago que hicieron, lo hicieron a otra cuenta y te hacen creer qu ya depositaron
+#de aqui ellos te presionan para decir que ya pagaron
+#https://www.youtube.com/watch?v=asFPM0B2yp
+
